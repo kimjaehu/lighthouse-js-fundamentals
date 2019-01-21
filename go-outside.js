1de108323f23ea70d709b7d5c936d1ae55d9902f
kimjaehu <kimjaehu@gmail.com>
2019-01-21 18:42:11 +0000

    Adds conditional excercises

---
+++ go-outside.js
@@ -0,0 +1,20 @@
+var raining = false;
+var temperature = 22;
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+if (!raining){
+  console.log("Leave your umbrella at home!")
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea...");
+} else {
+  console.log("Now you're ready to go outside!");
+}
